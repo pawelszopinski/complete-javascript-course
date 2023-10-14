77aeb893fe538b17f42cf1098f3f49b4d4caf955
Paweł Szopiński <szopinskipawel@gmail.com>
2023-10-14 14:50:41 +0200

    hold button and game ending

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -7,48 +7,85 @@ let currentScore0 = document.getElementById('current--0');
 let currentScore1 = document.getElementById('current--1');
 let player0 = document.querySelector('.player--0');
 let player1 = document.querySelector('.player--1');
-let scoreOne = document.getElementById('score--0');
-let scoreTwo = document.getElementById('score--1');
+let score0 = document.getElementById('score--0');
+let score1 = document.getElementById('score--1');
 
 // Initial phase
-scoreOne.textContent = 0;
-scoreTwo.textContent = 0;
+score0.textContent = 0;
+score1.textContent = 0;
 let playing = true;
 let results = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 diceResult.classList.add('hidden');
 
+const init = ()=>{
+score0.textContent = 0;
+score1.textContent = 0;
+playing = true;
+results = [0, 0];
+currentScore = 0;
+activePlayer = 0;
+diceResult.classList.add('hidden');
+}
+
+// Function switch active player
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0.classList.toggle('player--active');
+  player1.classList.toggle('player--active');
+};
+
 // Rolling dice
 rollBtn.addEventListener('click', () => {
-  // Generating number
-  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    // Generating number
+    const diceNumber = Math.trunc(Math.random() * 6) + 1;
 
-  // Displaying number
-  diceResult.src = `dice-${diceNumber}.png`;
-  diceResult.classList.remove('hidden');
+    // Displaying number
+    diceResult.src = `dice-${diceNumber}.png`;
+    diceResult.classList.remove('hidden');
 
-  // When result is 1 switch player
-  if (diceNumber !== 1) {
-    // Add dices
-    currentScore += diceNumber;
-    document.getElementById(`current--${activePlayer}`).textContent = currentScore
-  } else {
-    // switch
-    // change active player
-    
-    document.getElementById(`current--${activePlayer}`).textContent = 0
-    currentScore=0
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0.classList.toggle("player--active")
-    player1.classList.toggle("player--active")
- 
-    
+    // When result is 1 switch player
+    if (diceNumber !== 1) {
+      // Add dices
+      currentScore += diceNumber;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // switch
+      // change active player
 
-    
+      switchPlayer();
+    }
   }
 });
 
+holdBtn.addEventListener('click', () => {
+  if (playing) {
+    results[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      results[activePlayer];
+    if (results[activePlayer] >= 10) {
+      diceResult.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      playing = false;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+newBtn.addEventListener("click", ()=> {
+
+})
 // let arr = [];
 // let sum = 0;
 // let activePlayer = 0;
