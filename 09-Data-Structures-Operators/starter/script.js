81a7747f830a9c29452cc9953b199a17af2be1bd
Paweł Szopiński <szopinskipawel@gmail.com>
2024-01-29 17:55:00 +0100

    Assignment operators

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -46,3 +46,35 @@ console.log( guest1, guest2,guest3);
 // AND &&
 
 // console.log(1 && "Pawel");
+
+const rest1 = {
+  name: "Capri",
+  numGuests: 20,
+};
+
+const rest2 = {
+  name: "Milanese",
+  owner: "Giovanna Bianco",
+};
+
+// OR assignement operator (assign when left side is falsy)
+// rest1.numGuests = rest1.numGuests ||10;
+// rest2.numGuests = rest2.numGuests ||10;
+// rest1.numGuests||=10
+// rest2.numGuests||=10
+
+// OR coelescent operator(when null or undefined)
+rest1.numGuests??=10
+rest2.numGuests??=10
+
+// AND operator (assign value if it's truthy)
+// rest1.owner= rest1.owner && "Anonymous"
+// rest2.owner= rest2.owner && "Anonymous"
+// shorter
+rest1.owner &&="Anonymous"
+rest2.owner &&="Anonymous"
+
+
+
+console.log(rest1);
+console.log(rest2);
